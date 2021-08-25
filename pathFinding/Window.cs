f1072e45a68c27e3debdfcf07b073309f94ad4de
ra11p0 <p.lenczewski@icloud.com>
2021-08-25 20:31:54 +0200

    Dodaj pliki projektów.

---
+++ pathFinding/Window.cs
@@ -0,0 +1,161 @@
+﻿using System.Windows.Forms;
+using DragControlNamespace;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace pathFinding
+{
+    class Window : Form
+    {
+        private bool suspend = false;
+        private List<Node> nodes = new List<Node>();
+        private List<string> path = new List<string>();
+        public Window()
+        {
+            Button findPathButton = new Button();
+            findPathButton.Text = "Find path";
+            findPathButton.Click += (s, a) =>
+            {
+                Form dialog = new Form();
+                Button confirm = new Button() { Text = "confirm"};
+                ComboBox beginRoute = new ComboBox() { DropDownStyle = ComboBoxStyle.DropDownList };
+                ComboBox endRoute = new ComboBox() { DropDownStyle = ComboBoxStyle.DropDownList };
+                List<string> nodeNames = new List<string>();
+                nodes.ForEach(x => nodeNames.Add(x._name));
+                nodeNames.Sort();
+                beginRoute.Items.AddRange(nodeNames.ToArray());
+                endRoute.Items.AddRange(nodeNames.ToArray());
+                FlowLayoutPanel layout = new FlowLayoutPanel() {Dock = DockStyle.Fill};
+                confirm.Click += (cs, ca) =>
+                {
+                    try
+                    {
+                        path.Clear();
+                        path = PathManagement.Path.GetPath(beginRoute.Text, endRoute.Text, nodes);
+                        suspend = false;
+                        this.Refresh();
+                    }
+                    catch(System.FormatException ex) 
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                };
+                layout.Controls.AddRange(new Control[] { new Label() {Text = "Find route from: " }, beginRoute, new Label() {Text = "to: " }, endRoute, confirm});
+                dialog.Controls.Add(layout);
+                dialog.Show();
+            };
+            Button newNodeButton = new Button() { Text = "New node", Left = findPathButton.Right };
+            newNodeButton.Click += (s, a) =>
+              {
+                  Form dialog = new Form();
+                  Button confirm = new Button() { Text = "confirm" };
+                  TextBox name = new TextBox();
+                  FlowLayoutPanel layout = new FlowLayoutPanel() { Dock = DockStyle.Fill };
+                  ComboBox connectedWith = new ComboBox() { DropDownStyle = ComboBoxStyle.DropDownList };
+                  List<string> nodeNames = new List<string>();
+                  nodes.ForEach(x => nodeNames.Add(x._name));
+                  connectedWith.Items.AddRange(nodeNames.ToArray());
+                  nodeNames.Sort();
+                  confirm.Click += (cs, ca) =>
+                  {
+                      if (connectedWith.Text == "") PathManagement.AddPath(ref nodes, name.Text);
+                      else PathManagement.AddPath(ref nodes, name.Text, connectedWith.Text);
+                      DragControl newNodeDrag = new DragControl(this, nodes.Find(x => x._name == name.Text)._representation);
+                      this.Controls.Add(nodes.Find(x=> x._name == name.Text)._representation);
+                      nodes.Find(x => x._name == name.Text)._representation.BringToFront();
+                      nodes.Find(x => x._name == name.Text)._representation.MouseDown += (s, a) => suspend = true;
+                      nodes.Find(x => x._name == name.Text)._representation.MouseUp += (s, a) =>
+                      {
+                          suspend = false;
+                          this.Refresh();
+                      };
+                      suspend = false;
+                      this.Refresh();
+                      dialog.Close();
+                  };
+                  layout.Controls.AddRange(new Control[] { new Label() { Text = "Name of new node: ", Width = 120 }, name, new Label() { Text = "Connected with: " }, connectedWith, confirm });
+                  dialog.Controls.Add(layout);
+                  dialog.Show();
+              };
+            Button connectTwoNodes = new Button() { Text = "Connect nodes", Left = newNodeButton.Right };
+            connectTwoNodes.Click += (s, a) =>
+            {
+                Form dialog = new Form();
+                Button confirm = new Button() { Text = "confirm" };
+                ComboBox node = new ComboBox() { DropDownStyle = ComboBoxStyle.DropDownList };
+                FlowLayoutPanel layout = new FlowLayoutPanel() { Dock = DockStyle.Fill };
+                ComboBox connectedWith = new ComboBox() { DropDownStyle = ComboBoxStyle.DropDownList };
+                List<string> nodeNames = new List<string>();
+                nodes.ForEach(x => nodeNames.Add(x._name));
+                connectedWith.Items.AddRange(nodeNames.ToArray());
+                node.Items.AddRange(nodeNames.ToArray());
+                nodeNames.Sort();
+                confirm.Click += (cs, ca) =>
+                {
+                    PathManagement.AddPath(ref nodes, node.Text, connectedWith.Text);
+                    suspend = false;
+                    this.Refresh();
+                };
+                layout.Controls.AddRange(new Control[] { new Label() { Text = "Connect: "}, node, new Label() { Text = "with: " }, connectedWith, confirm });
+                dialog.Controls.Add(layout);
+                dialog.Show();
+            };
+
+            Width = 800;
+            Height = 600;
+            foreach (Node node in nodes)
+            {
+                DragControl dragNode = new DragControl(this, node._representation);
+                node._representation.MouseDown += (s, a) => suspend = true;
+                node._representation.MouseUp += (s, a) =>
+                   {
+                       suspend = false;
+                       this.Refresh();
+                   };
+                this.Controls.Add(node._representation);
+            }
+            this.Paint += (s, a) =>
+            {
+                Pen pen = new Pen(Color.Black, 3);
+                foreach (Node node in nodes)
+                {
+                    Point centerOfNode = new Point(node._representation.Left + node._representation.Width / 2, node._representation.Top + node._representation.Height / 2);
+                    foreach(string name in node._nodes)
+                    {
+                        List<Node> connectedNodes =  nodes.FindAll(x => x._name == name);
+                        foreach(Node connectedNode in connectedNodes)
+                        {
+                            Point centerOfConnectedNode = new Point(connectedNode._representation.Left + connectedNode._representation.Width / 2, connectedNode._representation.Top + connectedNode._representation.Height / 2);
+                            DrawLine(a, centerOfConnectedNode, centerOfNode, pen);
+                        }
+                    }
+                }
+                pen.Color = Color.Red;
+                foreach(string currentProcessedNodeName in path)
+                {
+                    try
+                    {
+                        if (nodes.Find(x => x._name == currentProcessedNodeName) == null) break;
+                        var firstNodeRepresentation = nodes.Find(x => x._name == currentProcessedNodeName)._representation;
+                        var secondNodeName = path[path.FindIndex(x => x == currentProcessedNodeName) + 1];
+                        var secondNodeRepresentation = nodes.Find(x => x._name == secondNodeName)._representation;
+                        Point firstNodePosition = new Point(firstNodeRepresentation.Left + firstNodeRepresentation.Width / 2, firstNodeRepresentation.Top + firstNodeRepresentation.Height / 2);
+                        Point secondNodePosition = new Point(secondNodeRepresentation.Left + secondNodeRepresentation.Width / 2, secondNodeRepresentation.Top + secondNodeRepresentation.Height / 2);
+                        DrawLine(a, firstNodePosition, secondNodePosition, pen);
+                    }
+                    catch(System.ArgumentOutOfRangeException ex)
+                    {
+                        break;
+                    }
+                        
+                }
+            };
+            this.Controls.AddRange(new Control[] { findPathButton, newNodeButton, connectTwoNodes });
+        }
+        public void DrawLine(PaintEventArgs e, Point start, Point end, Pen pen)
+        {
+            if (suspend) return;
+            e.Graphics.DrawLine(pen, start, end);
+        }
+    }
+}
---
+++ pathFinding/node.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace pathFinding
+{
+    public class Node
+    {
+        public List<string> _nodes = new List<string>();
+        public string _name { get; }
+        public Button _representation = new Button();
+        public Node(string name)
+        {
+            _name = name;
+            _representation.Text = name;
+        }
+        public void AddNode(string symbol)
+        {
+            _nodes.Add(symbol);
+        }
+    }
+    public static class PathManagement
+    {
+        public static void AddPath(ref List<Node> node, params string[] names)
+        {
+            foreach (string x in names)
+            {
+                if (node.Any(y => y._name == x))
+                {
+                    if (names.Length > names.ToList().FindIndex(z => z == x) + 1)
+                        node.Find(y => y._name == x)._nodes.Add(names[names.ToList().FindIndex(z => z == x) + 1]);
+                    if (0 <= names.ToList().FindIndex(z => z == x) - 1)
+                        node.Find(y => y._name == x)._nodes.Add(names[names.ToList().FindIndex(z => z == x) - 1]);
+                    node.Find(y => y._name == x)._nodes = node.Find(y => y._name == x)._nodes.Distinct().ToList();
+                }
+                else
+                {
+                    node.Add(new Node(x));
+                    if (names.Length > names.ToList().FindIndex(z => z == x) + 1)
+                        node.Find(y => y._name == x)._nodes.Add(names[names.ToList().FindIndex(z => z == x) + 1]);
+                    if (0 <= names.ToList().FindIndex(z => z == x) - 1)
+                        node.Find(y => y._name == x)._nodes.Add(names[names.ToList().FindIndex(z => z == x) - 1]);
+                }
+            }
+        }
+        static public class Path
+        {
+            public static List<string> GetPath(string first, string last, List<Node> nodes)
+            {
+                if (first == last) return new List<string>();
+                List<List<String>> firstTree = BuildTree(first, last, nodes);
+                if (firstTree.Count() == 0) return new List<string>();
+                return FindPath(GetCommonNodesFromTrees(firstTree, BuildTree(last, first, nodes)), nodes);
+            }
+            private static List<List<String>> BuildTree(string first, string last, List<Node> nodes)
+            {
+                List<List<string>> tree = new List<List<string>>();
+                List<String> visitedNodes = new List<string>();
+                tree.Add(new List<string> {first});
+                while(true)
+                {
+                    List<string> connectedNodes = new List<string>();
+                    foreach (String currentNode in tree.Last())
+                    {
+                        List<string> nodesToRemove = new List<string>();
+                        connectedNodes.AddRange(nodes.Find(x => x._name == currentNode)._nodes);
+                        visitedNodes.Add(currentNode);
+                        foreach (string connectedNode in connectedNodes) if(visitedNodes.Contains(connectedNode)) nodesToRemove.Add(connectedNode);
+                        foreach (string nodeToRemove in nodesToRemove) connectedNodes.Remove(nodeToRemove);
+                        visitedNodes = visitedNodes.Distinct().ToList();
+                    }
+                    tree.Add(connectedNodes.Distinct().ToList());
+                    if (connectedNodes.Count == 0) return new List<List<string>>();
+                    if (tree.Last().Contains(last)) break;
+                }
+                return tree;
+            }
+            private static List<List<String>> GetCommonNodesFromTrees(List<List<String>> firstTree, List<List<String>> secondTree)
+            {
+                if (firstTree.Count() != secondTree.Count())
+                {
+                    MessageBox.Show("Different size of trees.");
+                    return new List<List<string>>();
+                }
+                List<List<String>> outputTree = new List<List<string>>();
+                secondTree.Reverse();
+                int counter = 0;
+                foreach (List<string> currentLineFromFirstTree in firstTree)
+                {
+                    List<string> commonPart = new List<String>();
+                    List<string> currentLineFromSecondTree = secondTree[counter];
+                    counter++;
+                    foreach(String currentNodeInFirstLine in currentLineFromFirstTree)
+                    {
+                        if (currentLineFromSecondTree.Contains(currentNodeInFirstLine)) commonPart.Add(currentNodeInFirstLine);
+                    }
+                    outputTree.Add(commonPart);
+                }
+                return outputTree;
+            }
+            private static List<string> FindPath(List<List<String>> tree, List<Node> nodes)
+            {
+                List<string> path = new List<string>();
+                path.Add(tree[0][0]);
+                while (true)
+                {
+                    List<String> connectedToNode = nodes.Find(x => x._name == path.Last())._nodes;
+                    foreach (string node in connectedToNode)
+                    {
+                        if (tree[path.Count()].Contains(node))
+                        {
+                            path.Add(node);
+                            break;
+                        }
+                    }
+                    if (path.Contains(tree.Last().Last())) break;
+                }
+                return path;
+            }
+        }
+    }
+}
